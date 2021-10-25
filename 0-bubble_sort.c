f1984247f0629003ee31efc310c011a7c0dd5a5b
johanne101 <jlopez0493@gmail.com>
2021-10-25 23:39:11 +0000

    [Add] bubble sort function

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+/*
+* 0-bubble_sort.c - Bubble Sort
+* Date: October, 25, 2021
+* Author: Johanne J. Lopez
+*/
+
+#include "sort.h"
+
+/*
+ * bubble_sort -
+ * @array:
+ * @size:
+ */
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+// A function to implement bubble sort
+void bubble_sort(int *array, size_t size)
+{
+   size_t i, j;
+   for (i = 0; i < size-1; i++)
+   {
+	   /* Last i elements are already in place*/
+	   for (j = 0; j < size-i-1; j++)
+	   {
+		   if (array[j] > array[j+1])
+		   {
+			   swap(&array[j], &array[j+1]);
+			   print_array(array, size);
+		   }
+	   }
+   }
+}
